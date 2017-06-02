91df57bd7bfa3c5b81d2f7211183b13f0b69bc56
Chad Burkins <chad.burkins@gmail.com>
2017-06-01 21:07:16 -0400

    now using command-line args

--- pushoverListen.py
+++ pushoverListen.py
@@ -43,7 +43,6 @@ def getParamsFromFile(configFile, ConfigSection):
     if (not os.path.isfile(configFile)):
         print("Error: Config file '%s' doesn't exist" % configFile);
         exit();
-    
 
     # Read in the configuration file 
     Config = ConfigParser.ConfigParser()
@@ -64,35 +63,6 @@ def getParamsFromFile(configFile, ConfigSection):
 
     return (configDict)
 
-# ---------------------------------------------------------------------------------
-
-def backupFile (fileName):
-
-    import os
-    import shutil
-    import datetime
-    FilePath = fileName
-    modifiedTime = os.path.getmtime(FilePath) 
-    
-    timeStamp =  datetime.datetime.fromtimestamp(modifiedTime).strftime("%b-%d-%y-%H:%M:%S")
-    timeStamp =  datetime.datetime.now().strftime("%b-%d-%y-%H:%M:%S")
-
-    newFilePath = "backups/"+FilePath+"_"+timeStamp
-    shutil.copyfile(FilePath,newFilePath)
-
-# ---------------------------------------------------------------------------------
-
-def writeParamsToFile(configFile, ConfigSection, configDict):
-
-    #print ("\nWriting new config file\n")
-    with open(configFile, 'w') as configFile:
-        conf = ConfigParser.ConfigParser()
-        conf.add_section("fitbit");
-        for key,value in configDict.items():
-            conf.set('fitbit', key, value)
-            #print("New option: key=" + key + "  value=" + value)
-        conf.write(configFile)
-    
 # ---------------------------------------------------------------------------------
 #
 # Retrieves all outstanding messages from your PushOver queue
@@ -129,7 +99,6 @@ def getPushoverMessages(configDict):
 
 # ---------------------------------------------------------------------------------
 # Convert a Dictionary of messages over to one big string (within newlines between messages)
-#
 
 def MessagesToString(messageDict):
 
@@ -139,7 +108,6 @@ def MessagesToString(messageDict):
     # Join the list together into one big string separated by newlines
     return("\n".join(StringList) + "\n");
 
-    
 # ---------------------------------------------------------------------------------
 
 def deleteAllMessages(configDict):
@@ -147,7 +115,6 @@ def deleteAllMessages(configDict):
     messageDict = getPushoverMessages(configDict)
     lastMessageID = messageDict['messages'][lastIndex-1]['id']
     deleteMessagesByID(configDict, lastMessageID)
-
 
 # ---------------------------------------------------------------------------------
 
@@ -175,48 +142,11 @@ def deleteMessagesByID(configDict, message_id):
     c.setopt(c.URL, URL)
     c.setopt(c.POSTFIELDS, postfields)
 
-#    c.setopt(c.WRITEDATA, buffer)
     c.setopt(c.WRITEFUNCTION, buffer.write)
     c.perform()
     c.close()
     
     body = buffer.getvalue()
-
-    #vprint(body+"\n\n")
-
-# ---------------------------------------------------------------------------------
-
-def sendPushover(token, user, message):
-
-    import pycurl
-    from StringIO import StringIO
-    from urllib import urlencode
-
-    buffer = StringIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, 'https://api.pushover.net/1/messages.json')
-
-    post_data = {'token': token, 
-                 'user': user, 
-                 'device': 'iPhone6',
-                 'message': message}
-
-    # Form data must be provided already urlencoded.
-    postfields = urlencode(post_data)
-    # Sets request method to POST,
-    # Content-Type header to application/x-www-form-urlencoded
-    # and data to send in request body.
-    c.setopt(c.POSTFIELDS, postfields)
-
-#    c.setopt(c.WRITEDATA, buffer)
-    c.setopt(c.WRITEFUNCTION, buffer.write)
-    c.perform()
-    c.close()
-    
-    body = buffer.getvalue()
-    # Body is a string in some encoding.
-    # In Python 2, we can print it without knowing what the encoding is.
-    print(body)
 
 # ---------------------------------------------------------------------------------
 # ---------------------- Main -----------------------------------------------------
@@ -228,29 +158,32 @@ form = cgi.FieldStorage()
 verbose = False
 log = False
 
-# Check to see if we got called via command-line or HTTP GET
-if (form.keys() != []):
 
-    # Check for verbose flag
-    if (("verbose" in form) and (form['verbose'].value == 'true')):
-        verbose = True
-    # Check for log flag
-    if (("log" in form) and (form['log'].value == 'true')):
-        log = True
 
-    # Print a separator
-    vprint("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
 
-    # Looks like we got called via HTTP GET, and have at least one key
-    vprint("Keys from CGI FieldStorage\n");
-    for key in form.keys():
-        vprint("  Key=%s  Value=%s\n" % (key, form[key].value))
-    vprint("\n");
-
-else:
-    # We didn't get any keys from HTTP GET, maybe we got called via command line ?
-    vprint ("Keys: No keys\n")
-
+import getpass
+import argparse
+ 
+# Parse command-line arguments (Create ArgumentParser object)
+# By default, program name (shown in 'help' function) will be the same as the name of this file
+# Program name either comes from sys.argv[0] (invocation of this program) or from prog= argument to ArgumentParser
+# epilog= argument will be display last in help usage (strips out newlines)
+parser = argparse.ArgumentParser(description='Queries DataDomain appliances for relevant capacity statistics')
+ 
+# Test for verbose
+parser.add_argument('-v', dest='verbose', action='store_true', help='verbose_mode')
+ 
+# Get the object returned by parse_args
+args = parser.parse_args()
+verbose = args.verbose;
+ 
+# Prints command-line params
+vprint(" ")
+vprint("Command Line Parameters")
+vprint("---------------------------")
+vprint("verbose = %s" % args.verbose)
+vprint(" ")
+ 
 # Read config params from the config file
 configDict = getParamsFromFile(configFile, ConfigSection)
 
@@ -275,14 +208,14 @@ if (lastIndex > 0):
     lastIndex=len(messageDict['messages'])
     vprint("Number of messages = %d\n" % lastIndex)
 
-
-
 # OK, we've cleared the PushOver message queue
 # Create a websocket to listen for new messages
 import websocket
 import thread
 import time
 import logging
+
+# causes better errors to be printed from websocket
 logging.basicConfig();
 
 def on_message(ws, message):
@@ -311,7 +244,7 @@ def on_open(ws):
     thread.start_new_thread(run, ())
 
 if __name__ == "__main__":
-    # Enable verbose debugging on websocket connection
+    # Uncomment next line to enable verbose debugging on websocket connection
     # websocket.enableTrace(True)
     ws = websocket.WebSocketApp("wss://client.pushover.net/push",
                               on_message = on_message,
